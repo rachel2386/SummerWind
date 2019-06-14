08277f3f631f4d8892758147917853e7685fbd84
rachelqxl <rl2563@nyu.edu>
2019-06-14 10:31:49 -0400

    new characte abilities and animations:rolling, lying down, sitting, standing up

---
+++ Assets/0-SummerWind/Scripts/OpsiveAbilities/SitDown.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Opsive.UltimateCharacterController.Character.Abilities;
+
+public class SitDown : Ability
+{
+    // Start is called before the first frame update
+   
+}
