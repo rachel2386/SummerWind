9c7f9c84de4c6aecd031ec0f137d72f993fb91b6
rachelqxl <rl2563@nyu.edu>
2019-06-27 19:17:05 -0400

    level details: stone  table, bottle, cigarette butts, lotus, flower beds

---
+++ Assets/0-SummerWind/Scripts/FollowMouseMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using DG.Tweening;
+using HutongGames.PlayMaker.Actions;
+using UnityEngine;
+
+public class FollowMouseMovement : MonoBehaviour
+{
+    public bool followMouse = false;
+    //public Camera myCam;
+    private Vector3 MousePosInWorld;
+    public PlayMakerFSM CharacterFSM;
+
+    public float rotSpeed = 50;
+    private float smoother = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (followMouse)
+        {
+           Debug.Log("Rotating");
+            RotateObject();
+           
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                CharacterFSM.SendEvent("UserExit");
+               
+            }
+
+        }
+
+        void RotateObject()
+        {
+
+            float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+            float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+            Vector3 RotateAmount = Vector3.up * rotX + Vector3.right * rotY;
+            Tween rotateEase = transform.DORotate(RotateAmount, 0.5f, RotateMode.WorldAxisAdd);
+            rotateEase.SetEase(Ease.OutQuint);
+            
+//            transform.Rotate(Vector3.up,-rotX);
+//            transform.Rotate(Vector3.right,rotY);
+
+        }
+
+//        void FollowMouse()
+//        {
+//            Vector3 MousePos = Input.mousePosition;
+//            MousePos.z = 0.6f;
+//       
+//            Vector3 myPos = transform.position;
+//            myPos = myCam.ScreenToWorldPoint(MousePos);
+//            //myPos.y = transform.position.y;
+//            transform.position = Vector3.Lerp(transform.position,myPos,Time.deltaTime * 5f);
+//        }
+
+    }
+
+    
+}
