3dc1dc253c8cf424bedfda2b77049a114c736569
rachelqxl <rl2563@nyu.edu>
2019-07-16 18:35:25 +0800

    kite interactions. player hold kite and fly kite playmaker interactions

---
+++ Assets/0-SummerWind/Scripts/DrawKiteString.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawKiteString : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public Transform Kite;
+    public Transform Handle;
+    public Material StringMat;
+    private LineRenderer myLine;
+    
+   
+    void Start()
+    {
+        myLine = gameObject.AddComponent<LineRenderer>();
+        myLine.material = StringMat;
+        myLine.startWidth = 0.003f;
+        print(myLine.startWidth);
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myLine.SetPosition(0, Kite.position);
+        myLine.SetPosition(1, Handle.position);
+    }
+}
