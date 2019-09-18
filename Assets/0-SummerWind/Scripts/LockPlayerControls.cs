bea2717532205949fe4f49145c74df7b6546f6e4
rachelqxl <rl2563@nyu.edu>
2019-09-18 01:33:50 -0400

    timeline stuff

---
+++ Assets/0-SummerWind/Scripts/LockPlayerControls.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Opsive.UltimateCharacterController.Character;
+using Opsive.UltimateCharacterController.Input;
+using UnityEngine;
+
+public class LockPlayerControls : MonoBehaviour
+{
+    private UltimateCharacterLocomotionHandler playerControl;
+
+    private UnityInput MouseLook;
+
+    public bool LockPlayerInput = false;
+
+    public bool LockMouseLook = false;
+    public bool LockAllControls = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControl = GetComponent<UltimateCharacterLocomotionHandler>();
+        MouseLook = GetComponent<UnityInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerControl.enabled = !LockPlayerInput;
+        MouseLook.enabled = !LockMouseLook;
+
+        if (LockAllControls)
+        {
+            LockMouseLook = true;
+            LockPlayerInput = true; 
+        }
+        else
+        {
+            LockMouseLook = false;
+            LockPlayerInput = false; 
+        }
+       
+
+    }
+
+    public void LockAllPlayerInput()
+    {
+        LockAllControls = true;
+    }
+   
+    public void UnlockAllPlayerInput()
+    {
+        LockAllControls = false;
+    }
+}
