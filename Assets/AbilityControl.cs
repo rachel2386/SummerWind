1a0a5fadd3cde7fdd743c3b1d44d1d9a8037bb64
rachelqxl <rl2563@nyu.edu>
2019-09-26 19:43:06 -0400

    combined sit roll stand abilities

---
+++ Assets/AbilityControl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using Opsive.UltimateCharacterController.Character;
+using Opsive.UltimateCharacterController.Character.Abilities;
+
+public class AbilityControl : MonoBehaviour
+{
+    public string AbilityName;
+    private UltimateCharacterLocomotion characterLocomotion;
+
+    public bool stopAbility = false;
+    private void Start()
+    {
+        characterLocomotion = GetComponent<UltimateCharacterLocomotion>();
+        var ability = characterLocomotion.GetAbility<SitDown>();
+     
+    }
+    // Tries to start the jump ability. There are many cases where the ability may not start, 
+    // such as if it doesn't have a high enough priority or if CanStartAbility returns false.
+    private void Update()
+    {
+        if (stopAbility)
+        {
+            StopAbility();
+            stopAbility = false;
+        }
+    }
+
+
+    public void StartAbility()
+    {
+        characterLocomotion.TryStartAbility(characterLocomotion.GetAbility<SitDown>());
+
+    }
+    
+    // Stop the jump ability if it is active.
+    public void StopAbility( )
+    {
+        var ability = characterLocomotion.GetAbility<SitDown>();
+        
+        if (ability.IsActive) {
+            characterLocomotion.TryStopAbility(ability); 
+        }
+        
+    }
+}
